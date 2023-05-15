40517ef1ed56ac2ecf9ed7a62131fd17de86f629
Dioxane <gordonkox@tuta.io>
2023-05-15 18:06:07 +0300

    context moved to commander

--- internal/bot/command/newCommander.go
+++ internal/bot/command/newCommander.go
@@ -1,24 +1,26 @@
 package command
 
 import (
+	"context"
+
 	"github.com/JackBekket/telegram-gpt/internal/database"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Commander struct {
-	bot         *tgbotapi.BotAPI
-	userDb      map[int64]database.User
-	aiSessionDb map[int64]database.AiSession
+	bot     *tgbotapi.BotAPI
+	usersDb map[int64]database.User
+	ctx     context.Context
 }
 
 func NewCommander(
 	bot *tgbotapi.BotAPI,
-	userDb map[int64]database.User,
-	sessionDB map[int64]database.AiSession,
+	usersDb map[int64]database.User,
+	ctx context.Context,
 ) *Commander {
 	return &Commander{
-		bot:         bot,
-		userDb:      userDb,
-		aiSessionDb: sessionDB,
+		bot:     bot,
+		usersDb: usersDb,
+		ctx:     ctx,
 	}
 }
