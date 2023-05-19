78aa5bfcf8bbabdbea86dc6adc982717d4d55b4c
Dioxane <gordonkox@tuta.io>
2023-05-19 14:53:44 +0300

    The function has been simplified.

--- internal/openaibot/createClient.go
+++ internal/openaibot/createClient.go
@@ -1,20 +1,9 @@
 package openaibot
 
 import (
-	db "github.com/JackBekket/telegram-gpt/internal/database"
 	gogpt "github.com/sashabaranov/go-openai"
 )
 
-func CreateClient(gptKey string, chatID int64, modelName string) *gogpt.Client {
-
-	client := gogpt.NewClient(gptKey)
-	db.UsersMap[chatID] = db.User{
-		AiSession: db.AiSession{
-			GptKey:    gptKey,
-			GptClient: *client,
-			GptModel:  modelName,
-		},
-	}
-
-	return client
+func CreateClient(gptKey string) *gogpt.Client {
+	return gogpt.NewClient(gptKey)
 }
